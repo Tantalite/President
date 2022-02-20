73be49ead5275ec25ff82cdf67f34e0ab69ac589
Lucky sedi <sediose01@gmail.com>
2022-02-20 05:31:38 +0000

    second

--- Pres.java
+++ Pres.java
@@ -1,3 +1,6 @@
 Hello bro
 What do you think of the president's speech
 I do not agree with his approach to governance.
+We should meet urgently to discuss my prospect further
+I need to run for the presidency
+
