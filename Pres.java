f7ec15e7218ddba307f401601aa398a5331606c8
Lucky sedi <sediose01@gmail.com>
2022-02-24 02:06:08 +0000

    2-23-2022 second

--- Pres.java
+++ Pres.java
@@ -4,5 +4,6 @@ I do not agree with his approach to governance.
 We should meet urgently to discuss my prospect further
 I need to run for the presidency
 After careful consideration of the events of the last months
-I, Oloye Lucky Sedi wish to declare my candidacy for the Akoko-Edo north senitorial race.
+I, Oloye Lucky Sedi 11  wish to declare my candidacy for the Akoko-Edo north senitorial race.
 This stand was reach through deep cogitation and due consultations with stakeholders, elder statement, politician across board.
+
