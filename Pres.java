3ce81c6b05227b91704793c512bbd77a0e5e656e
Lucky sedi <sediose01@gmail.com>
2022-02-24 01:30:14 +0000

    commit 2-23-2022

--- Pres.java
+++ Pres.java
@@ -3,4 +3,6 @@ What do you think of the president's speech
 I do not agree with his approach to governance.
 We should meet urgently to discuss my prospect further
 I need to run for the presidency
-
+After careful consideration of the events of the last months
+I, Oloye Lucky Sedi wish to declare my candidacy for the Akoko-Edo north senitorial race.
+This stand was reach through deep cogitation and due consultations with stakeholders, elder statement, politician across board.
